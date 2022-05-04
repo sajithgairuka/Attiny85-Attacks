7166d4747939d660c7c08ebf836c75d192f7810f
Sajith Gairuka Jayaweera <104394666+sajithgairuka@users.noreply.github.com>
2022-05-05 03:36:52 +0530

    Update fakeupdate.ino

--- fakeupdate/fakeupdate.ino
+++ fakeupdate/fakeupdate.ino
@@ -7,11 +7,11 @@ void loop() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(600);
-  DigiKeyboard.print("https://youtu.be/_nQU_8Nm0Yk?t=4550");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(5000);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(3000);
+  //DigiKeyboard.print("https://youtu.be/_nQU_8Nm0Yk?t=4550");
+  //DigiKeyboard.sendKeyStroke(KEY_ENTER);
+ // DigiKeyboard.delay(5000);
+ // DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+ // DigiKeyboard.delay(3000);
   DigiKeyboard.print("http://fakeupdate.net/win10ue");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(2000);
